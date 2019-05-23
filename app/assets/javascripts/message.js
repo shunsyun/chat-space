97fda36d5931f851c5de39c6068514320d770b38
shunsyun <shunsukesawai0422@gmail.com>
2019-05-23 21:05:43 +0900

    update message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,5 @@
 $(function(){
   function buildHTML(message){
-    // if ( message.image ) {
     var addImage = (message.image !== null) ? `<img src=${message.image} >` : ''
     var html = `<div class ="message" id = ${message.id}>
                   <div class ="message__top-info">
@@ -12,20 +11,8 @@ $(function(){
                 </div>`
     return html;
    }  
-  //  else {
-  //   var html = `<div class ="message" id = ${message.id}>
-  //                 <div class ="message__top-info">
-  //                   <p class="message__top-info__user-name">${message.user_name}</p>
-  //                   <p class="message__top-info__date">${message.date}</p>
-  //                 </div>
-  //                 <p class="message__message-text">${message.content}</p>
-  //               </div>`
-  //   return html;
-  //   };
-  // }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    // console.log(this)
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
@@ -39,7 +26,6 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('.messages').val('')
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       $('form')[0].reset();
     })
